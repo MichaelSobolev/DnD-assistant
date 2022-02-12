5307dfc205390a26900780deebc474c18ff5c444
MichaelSobolev <alsm16.wm@gmail.com>
2022-02-12 17:06:35 +0300

    ADD: last sheet preview

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -5,21 +5,30 @@ const { Sheet } = require('../db/models')
 const router = express.Router();
 const loginAuth = require('../middlewares/login');
 
+const dateSortCallback = (first, second) => {
+  var firstDate = new Date(first.updatedAt);
+  var secondDate = new Date(second.updatedAt);
+  return secondDate - firstDate;
+}
+
 router.get('/', loginAuth, async (req, res) => {
   let rules = (await dndApi('https://www.dnd5eapi.co/api/rule-sections/')).results
   try {
-    const userSheets = await Sheet.findAll({
+    let userSheets = await Sheet.findAll({
       where: {
         user_id: req.session.userId
       },
       raw: true,
     })
-    res.render('index', { rules, userSheets })
+    const lastSheet = userSheets.pop()
+    userSheets = userSheets.sort(dateSortCallback)
+    console.log(lastSheet)
+
+    res.render('index', { rules, userSheets, lastSheet })
   } catch (err) {
     res.render('login')
   }
 });
-
 
 
 module.exports = router;
